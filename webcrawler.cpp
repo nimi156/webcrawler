64eedf1427c1a7c58993d937a6b4b1e7ae36580a
Haoran Wang <wang2226@cuda.cs.purdue.edu>
2017-07-15 08:36:00 -0400

    Commit web-crawler

--- webcrawler.cpp
+++ webcrawler.cpp
@@ -181,8 +181,8 @@ WebCrawler::onContentFound(char c){
 			}
 		}
 	} else if (c == (char)18){ //sign of description end
-printf("i=%d, size=%d, url=%s, desc=%s\n",_headURL, (int)strlen(_urlArray[_headURL]._description), _urlArray[_headURL]._url, _urlArray[_headURL]._description);
-fflush(stdout);
+//printf("i=%d, size=%d, url=%s, desc=%s\n",_headURL, (int)strlen(_urlArray[_headURL]._description), _urlArray[_headURL]._url, _urlArray[_headURL]._description);
+//fflush(stdout);
 		memset(descrip, 0, sizeof(char)*strlen(descrip));
 		wordBuff = descrip;
 	} else if (c == '"'){
