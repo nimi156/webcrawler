232ade14449dca0e83000ff5e6fe1122375cbca6
Haoran Wang <wang2226@cuda.cs.purdue.edu>
2017-07-15 08:05:48 -0400

    Commit web-crawler

--- webcrawler.cpp
+++ webcrawler.cpp
@@ -275,7 +275,7 @@ WebCrawler::writeURLFile(const char * urlFileName){
 		fprintf(fp, "%d %s\n", i, _urlArray[i]._url);
 		//write its description
 		int j = 0;
-printf("i=%d, desc=%s\n",i, _urlArray[i]);
+printf("i=%d, url=%s, desc=%s\n",i, _urlArray[i]._url, urlArray[i]._description);
 fflush(stdout);
 		while(_urlArray[i]._description[j] != '\0'){
 			char c;
